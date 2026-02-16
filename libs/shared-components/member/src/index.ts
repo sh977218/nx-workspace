3f073ca6e2e65f59a3b29250cc6d63a3bc3a4048
Peter Huang <peterhuang1113@gmail.com>
2026-02-16 10:42:12 -0500

    revert

--- libs/shared-components/member/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/member.component';
-export * from './lib/member.dialog';
-export * from './lib/member-module';
--- libs/shared-components/squad/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/squad.component';
-export * from './lib/squad-dialog.component';
-export * from './lib/squad-module';
