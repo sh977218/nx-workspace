245db75cf0db8f7c7ece6d4414996a74e4e342c0
Peter Huang <peterhuang1113@gmail.com>
2026-02-16 20:06:42 -0500

    fix(ui): fix storybook

--- libs/shared-components/.storybook/main.ts
+++ libs/shared-components/.storybook/main.ts
@@ -1,10 +1,12 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
   addons: [],
   framework: {
-    name: '@storybook/angular',
+    name: getAbsolutePath("@storybook/angular"),
     options: {},
   },
 };
@@ -14,3 +16,7 @@ export default config;
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
 // and https://nx.dev/recipes/storybook/custom-builder-configs
+
+function getAbsolutePath(value: string): any {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
