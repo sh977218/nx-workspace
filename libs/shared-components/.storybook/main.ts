e63f8cdeb11f415c3340b8ce2abae3b552597782
Peter Huang <peterhuang1113@gmail.com>
2026-02-16 20:38:50 -0500

    fix lint

--- libs/shared-components/.storybook/main.ts
+++ libs/shared-components/.storybook/main.ts
@@ -17,6 +17,7 @@ export default config;
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
 // and https://nx.dev/recipes/storybook/custom-builder-configs
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function getAbsolutePath(value: string): any {
   return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
 }
