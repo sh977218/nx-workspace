32b75f199e8b7e661dc376ad3bb20f88b2567af9
Peter Huang <peterhuang1113@gmail.com>
2026-02-18 21:38:42 -0500

    feat(ui & api): setup chat using socket io

--- apps/ui/src/app/app.ts
+++ apps/ui/src/app/app.ts
@@ -1,7 +1,9 @@
 import { NgClass } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 
+import { ChatDialogComponent } from './chat-dialog/chat-dialog.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MaterialModule } from './material.module';
 import { ThemeService } from './theme-service';
@@ -9,8 +11,13 @@ import { ThemeService } from './theme-service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [FormsModule, NgClass, NavBarComponent, MaterialModule],
+  imports: [FormsModule, NgClass, NavBarComponent, MaterialModule]
 })
 export class App {
+  readonly dialog = inject(MatDialog);
   themeService = inject(ThemeService);
+
+  openMemberDetailDialog() {
+    this.dialog.open(ChatDialogComponent, { height: '1000px', width: '600px' });
+  }
 }
---
+++ apps/ui/src/app/chat-dialog/chat-dialog.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+
+import { MaterialModule } from '../material.module';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  imports: [MaterialModule, FormsModule],
+  template: `
+    <h2 matDialogTitle>Chat</h2>
+    <mat-dialog-content>
+      <ul class="flex flex-col gap-2 p-3 border-2 border-gray-300 rounded-lg">
+        @for (message of messages; track message) {
+          <li class="flex align-baseline odd:bg-white even:bg-gray-50">
+            <mat-icon>person</mat-icon>
+            {{ message }}
+          </li>
+        }
+      </ul>
+    </mat-dialog-content>
+    <mat-dialog-actions>
+      <mat-form-field class="w-full">
+        <mat-label>Type Messages</mat-label>
+        <textarea matInput [(ngModel)]="message"></textarea>
+      </mat-form-field>
+      <button matButton cdkFocusInitial (click)="sendMessage()">
+        Send
+      </button>
+      <button matButton matDialogClose>Close</button>
+    </mat-dialog-actions>
+  `,
+})
+export class ChatDialogComponent implements OnInit {
+  private socket: Socket;
+  message = '';
+  messages: string[] = [];
+
+  constructor() {
+    this.socket = io('http://localhost:3000'); // Connect to Socket.IO server
+  }
+
+  ngOnInit(): void {
+    this.socket.on('events', (msg) => {
+      this.messages.push(msg);
+    });
+  }
+
+  sendMessage(): void {
+    this.socket.emit('events', this.message);
+  }
+}
