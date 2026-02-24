5d21afa2ecc90f2498d1e7815cb91321701b51c2
Peter Huang <peterhuang1113@gmail.com>
2026-02-23 19:46:56 -0500

    feat(ui & api): socket io with room

--- apps/ui/src/app/chat-dialog/chat-dialog.component.ts
+++ apps/ui/src/app/chat-dialog/chat-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { io, Socket } from 'socket.io-client';
 
@@ -10,8 +10,10 @@ import { MaterialModule } from '../material.module';
     <h2 matDialogTitle>Chat</h2>
     <mat-dialog-content>
       <ul class="flex flex-col gap-2 p-3 border-2 border-gray-300 rounded-lg">
-        @for (message of messages; track message) {
-          <li class="flex align-baseline odd:bg-white even:bg-gray-50">
+        @for (message of messages(); track $index) {
+          <li
+            class="flex align-baseline odd:bg-white even:bg-gray-50 even:flex-row-reverse"
+          >
             <mat-icon>person</mat-icon>
             {{ message }}
           </li>
@@ -23,9 +25,7 @@ import { MaterialModule } from '../material.module';
         <mat-label>Type Messages</mat-label>
         <textarea matInput [(ngModel)]="message"></textarea>
       </mat-form-field>
-      <button matButton cdkFocusInitial (click)="sendMessage()">
-        Send
-      </button>
+      <button matButton cdkFocusInitial (click)="sendMessage()">Send</button>
       <button matButton matDialogClose>Close</button>
     </mat-dialog-actions>
   `,
@@ -33,19 +33,32 @@ import { MaterialModule } from '../material.module';
 export class ChatDialogComponent implements OnInit {
   private socket: Socket;
   message = '';
-  messages: string[] = [];
+  roomName = 'some room';
+  messages = signal<string[]>([]);
 
   constructor() {
     this.socket = io('http://localhost:3000'); // Connect to Socket.IO server
+
+    this.socket.on('connect', () => {
+      this.socket.emit('joinRoom', {
+        roomName: this.roomName,
+        userId: 'user1',
+      });
+    });
   }
 
   ngOnInit(): void {
-    this.socket.on('events', (msg) => {
-      this.messages.push(msg);
+    this.socket.on('msgToClient', (data) => {
+      this.messages.update((messages) => [...messages, data.message]);
     });
   }
 
   sendMessage(): void {
-    this.socket.emit('events', this.message);
+    this.socket.emit('sendMessageToRoom', {
+      roomName: this.roomName,
+      message: this.message,
+      sender: 'user1',
+    });
+    this.message = '';
   }
 }
