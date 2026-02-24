191b9c22c97002abd6366071e83e369b6db1371d
Peter Huang <peterhuang1113@gmail.com>
2026-02-23 20:38:41 -0500

    feat(ui): add user login (#390)

--- apps/ui/src/app/nav-bar/nav-bar.component.ts
+++ apps/ui/src/app/nav-bar/nav-bar.component.ts
@@ -2,12 +2,16 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
+import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
 import { MaterialModule } from '../material.module';
-import { ThemeService } from '../theme-service';
+import { ThemeService } from '../theme.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -19,38 +23,57 @@ import { ThemeService } from '../theme-service';
     RouterLinkActive,
     RouterLink,
     FormsModule,
-    MaterialModule,
-  ],
+    MaterialModule
+  ]
 })
 export class NavBarComponent {
+  readonly dialog = inject(MatDialog);
   themeService = inject(ThemeService);
+  userService = inject(UserService);
+  _snackBar = inject(MatSnackBar);
+
   routes = [
     {
       path: 'search',
-      label: 'Search',
+      label: 'Search'
     },
     {
       path: 'threeJs',
-      label: 'Three JS',
+      label: 'Three JS'
     },
     {
       path: 'dashboard',
-      label: 'Dashboard',
+      label: 'Dashboard'
     },
     {
       path: 'video',
-      label: 'Video',
+      label: 'Video'
     },
     {
       path: 'feed',
-      label: 'Feed',
-    }    
+      label: 'Feed'
+    }
   ];
   private breakpointObserver = inject(BreakpointObserver);
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
       map((result) => result.matches),
-      shareReplay(),
+      shareReplay()
     );
+
+  openLoginDialog() {
+    this.dialog
+      .open(LoginDialogComponent, {
+        height: '1000px',
+        width: '600px'
+      })
+      .afterClosed()
+      .subscribe((result) => {
+        if (result) {
+          this.userService.loggedInUser = result;
+          this._snackBar.open(`${result.name} logged in.`, 'Close');
+        }
+      });
+  }
 }
