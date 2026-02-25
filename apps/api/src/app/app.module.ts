bc549437755c810ddf7321d2ba02b10e3108c017
Peter Huang <peterhuang1113@gmail.com>
2026-02-25 14:27:25 -0500

    fix lint

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import * as Joi from 'joi';
 
+import { AuthModule } from './auth/auth.module';
 import { DataLoadModule } from './data-load/data-load.module';
 import { EventsModule } from './events/events.module';
 import { FeedModule } from './feed/feed.module';
@@ -10,7 +11,6 @@ import { SquadModule } from './squad/squad.module';
 import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 
 const ENV = process.env.NODE_ENV;
 
--- apps/api/src/app/user/user.controller.ts
+++ apps/api/src/app/user/user.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { UserService } from './user.service';
 
