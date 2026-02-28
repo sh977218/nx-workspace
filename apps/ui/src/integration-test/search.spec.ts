e75cb954719c00b4c5727e1f2c24de41dcd5e78d
Peter Huang <peterhuang1113@gmail.com>
2026-02-27 21:35:34 -0500

    test(ui): try vitest browser page object model (#441)

--- apps/ui/src/integration-test/search.spec.ts
+++ apps/ui/src/integration-test/search.spec.ts
@@ -1,24 +1,12 @@
-import { Component } from '@angular/core';
 import { expect, test } from 'vitest';
 import { render } from 'vitest-browser-angular';
 
 import { SearchComponent } from '../app/search/search.component';
 
-@Component({
-  template: `
-    <app-search /> `,
-  imports: [
-    SearchComponent
-  ]
-})
-export class MyComponent {
-}
-
 test('query elements', async () => {
   // Render the component
-  const result = await render(MyComponent);
+  const result = await render(SearchComponent);
 
-  // Test that the search form is rendered
   const formElement = result.container.querySelector('form');
-  await expect(formElement).toBeVisible();
+  expect(formElement).toBeVisible();
 });
