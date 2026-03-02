98e4fae10a4a952d671d62374ffb68f82837f51a
Peter Huang <peterhuang1113@gmail.com>
2026-03-01 19:58:28 -0500

    fix(ui): fix logout (#454)

--- apps/e2e/src/fixtures/login.fixture.ts
+++ apps/e2e/src/fixtures/login.fixture.ts
@@ -18,5 +18,11 @@ export const loginFixture = baseTest.extend<{
     await loginPage.userItem.filter({ hasText: username }).click();
     await loginPage.loginButton.click();
     await use(loginPage);
+    await loginPage.header.profileButton.click();
+    await loginPage.header.logoutMenu.click();
+    await expect(
+      loginPage.material.snackBar.getByText('You have been logged out.'),
+    ).toBeVisible();
+
   }
 });
--- libs/shared-models/src/lib/page-object-model/login.po.ts
+++ libs/shared-models/src/lib/page-object-model/login.po.ts
@@ -1,14 +1,17 @@
 import { Locator, Page } from '@playwright/test';
 
+import { HeaderPo } from './header.po';
 import { MaterialPo } from './material.po';
 
 export class LoginPo {
   readonly material: MaterialPo;
+  readonly header: HeaderPo;
   readonly userList: Locator;
   readonly userItem: Locator;
   readonly loginButton: Locator;
   constructor(private readonly page: Page) {
     this.material = new MaterialPo(page);
+    this.header = new HeaderPo(page);
     this.userList = this.page.getByRole('listbox');
     this.userItem = this.page.getByRole('listbox').getByRole('option');
     this.loginButton = this.page.getByRole('button', { name: 'Login' });
