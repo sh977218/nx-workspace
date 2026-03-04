440b5f63d23836ac7c9f9c877b96aa1e303c1c7f
Peter Huang <peterhuang1113@gmail.com>
2026-03-03 21:45:27 -0500

    a

--- apps/auth-api/src/app/app.controller.ts
+++ apps/auth-api/src/app/app.controller.ts
@@ -51,20 +51,18 @@ export class AppController {
     const payload = { sub: user.id, username: user.username };
     const jwt = await this.jwtService.signAsync(payload);
     res.cookie('jwt', jwt, {
-      domain: 'http://localhost'
+      domain: 'localhost'
     });
-    res.redirect('http://localhost:4200');
-  /*  return res.status(HttpStatus.OK).send({
+    return res.status(HttpStatus.OK).send({
       jwt,
       user
     });
-    */
   }
 
   @HttpCode(HttpStatus.OK)
   @Post('logout')
   logout(@Res() res: Response) {
-    res.clearCookie('jwt', { domain: 'http://localhost' });
+    res.clearCookie('jwt', { domain: 'localhost' });
     return res.status(200).send();
   }
 }
