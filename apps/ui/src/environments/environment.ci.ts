0b506548be8d9ae2c63f928f6264fc45c601f5f1
Peter Huang <peterhuang1113@gmail.com>
2026-03-09 22:32:59 -0400

    try this

--- apps/ui/src/environments/environment.ci.ts
+++ apps/ui/src/environments/environment.ci.ts
@@ -2,7 +2,7 @@ import { EnvironmentModel } from './environment.model';
 
 export const environment: EnvironmentModel = {
   production: false,
-  api: 'http://localhost:3000',
+  api: 'http://localhost:3000/api',
   rssfeed: 'http://rss.cnn.com/rss/cnn_topstories.rss',
 //  authApi: 'http://localhost:4000',
   idpUrl: 'http://localhost:5200',
