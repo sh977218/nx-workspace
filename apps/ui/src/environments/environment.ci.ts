6e1a41d7e97d0b90551f72bb1d6239845e4445da
Peter Huang <peterhuang1113@gmail.com>
2026-04-05 11:11:00 -0400

    refactor(ui): remove ai & autho-ui

--- apps/ui/src/environments/environment.ci.ts
+++ apps/ui/src/environments/environment.ci.ts
@@ -5,5 +5,4 @@ export const environment: EnvironmentModel = {
   api: 'http://localhost:3000/api',
   rssfeed: 'http://rss.cnn.com/rss/cnn_topstories.rss',
   //  authApi: 'http://localhost:4000',
-  idpUrl: 'http://localhost:5200',
 };
